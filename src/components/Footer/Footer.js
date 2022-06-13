2fd1808cf0a7cfea728df51a2c5808b71665731b
Godwinjose <godwinjoshwa.s@colanonline.com>
2022-06-13 16:47:59 +0530

    dashboard footer changes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,20 @@
 import { Grid } from "@material-ui/core";
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function Footer() {
+  const location = useLocation();
+  console.log(location);
   return (
     <div>
-      <Grid className="footer">
-        <p className="foot_text">
+      <Grid
+        className={location.pathname === "/SwabK" ? "footer_swab" : "footer"}
+      >
+        <p
+          className={
+            location.pathname === "/SwabK" ? "foot_text_swab" : "foot_text"
+          }
+        >
           Copyrights © 2022{" "}
           <span style={{ fontWeight: "bolder", color: "#000" }}> Genetix</span>.
           All Rights Reserved
--- src/components/navbarContent/Nav.js
+++ src/components/navbarContent/Nav.js
@@ -17,13 +17,13 @@ import downarrow from "../img&Logos/downarrow.png";
 import ManPat from "../navbar/ManPat";
 import SwabK from "../navbar/SwabK";
 import Dashboard from "../navbar/Dashboard";
-import '../navbarCSS/dashboard.css'
+import "../navbarCSS/dashboard.css";
 import { useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 function Nav() {
-  const history = useHistory()
-  const location = useLocation()
+  const history = useHistory();
+  const location = useLocation();
   const [dash, setDash] = useState(false);
   const [mancont, setMancont] = useState(false);
   const [manemp, setManemp] = useState(false);
@@ -35,15 +35,15 @@ function Nav() {
   const [swak, setSwak] = useState(false);
 
   const navHandle = () => {
-    history.push('/Dashboard')
+    history.push("/Dashboard");
   };
 
   const navHandle1 = () => {
-    history.push('/ManangeContractors')
+    history.push("/ManangeContractors");
   };
 
   const navHandle2 = () => {
-    history.push('/ManangeEmployees')
+    history.push("/ManangeEmployees");
   };
 
   const navHandle3 = () => {
@@ -59,31 +59,28 @@ function Nav() {
   };
 
   const navHandle4 = () => {
-    history.push('/ManangeReports')
+    history.push("/ManangeReports");
   };
 
   const navHandle5 = () => {
-    history.push('/ManangeForms')
+    history.push("/ManangeForms");
   };
 
   const navHandle6 = () => {
-    history.push('/ManangeLabs')
+    history.push("/ManangeLabs");
   };
 
   const navHandle7 = () => {
-    history.push('/Manangephysician')
+    history.push("/Manangephysician");
   };
 
-  
   const navHandle8 = () => {
-    history.push('/ManangePatients')
+    history.push("/ManangePatients");
   };
 
-    
   const navHandle9 = () => {
-    history.push('/SwabK')
+    history.push("/SwabK");
   };
-
 
   return (
     <div className="nav">
@@ -104,199 +101,296 @@ function Nav() {
 
         <Grid className="box">
           <Grid className="boxitems">
-            <Grid className={location.pathname === "/Dashboard" ? "dash" : "non_dash"} onClick={navHandle}>
-              <p className={location.pathname === "/Dashboard" ? "dash_text"  : "nondash_text"}>Dashboard</p>
+            <Grid
+              className={
+                location.pathname === "/Dashboard" ? "dash" : "non_dash"
+              }
+              onClick={navHandle}
+            >
+              <p
+                className={
+                  location.pathname === "/Dashboard"
+                    ? "dash_text"
+                    : "nondash_text"
+                }
+              >
+                Dashboard
+              </p>
             </Grid>
             <Grid className="line" />
 
-            <Grid className={location.pathname === "/ManangeContractors" ? "mc" : "non_mc"} onClick={navHandle1}>
-              <p className={location.pathname === "/ManangeContractors" ? "mc_text" : "nonmc_text"}>
+            <Grid
+              className={
+                location.pathname === "/ManangeContractors" ? "mc" : "non_mc"
+              }
+              onClick={navHandle1}
+            >
+              <p
+                className={
+                  location.pathname === "/ManangeContractors"
+                    ? "mc_text"
+                    : "nonmc_text"
+                }
+              >
                 Manage Contractors
               </p>
             </Grid>
             <Grid className="line1" />
 
-            <Grid className={location.pathname === "/ManangeEmployees" ? "me" : "non_me"} onClick={navHandle2}>
-              <p className={location.pathname === "/ManangeEmployees" ? "me_text" : "nonme_text"}>
+            <Grid
+              className={
+                location.pathname === "/ManangeEmployees" ? "me" : "non_me"
+              }
+              onClick={navHandle2}
+            >
+              <p
+                className={
+                  location.pathname === "/ManangeEmployees"
+                    ? "me_text"
+                    : "nonme_text"
+                }
+              >
                 Manage Employees
               </p>
             </Grid>
             <Grid className="line2" />
 
-            <Grid className={location.pathname === "/ManangeReports" ? "mr" : "non_mr"} onClick={navHandle4}>
-              <p className={location.pathname === "/ManangeReports" ? "mr_text" : "nonmr_text"}>
+            <Grid
+              className={
+                location.pathname === "/ManangeReports" ? "mr" : "non_mr"
+              }
+              onClick={navHandle4}
+            >
+              <p
+                className={
+                  location.pathname === "/ManangeReports"
+                    ? "mr_text"
+                    : "nonmr_text"
+                }
+              >
                 Manage Reports
               </p>
             </Grid>
             <Grid className="line3" />
 
-            <Grid className={location.pathname === "/ManangeForms" ? "mf" : "non_mf"} onClick={navHandle5}>
-              <p className={location.pathname === "/ManangeForms" ? "mf_text" : "nonmf_text"}>Manage Forms</p>
+            <Grid
+              className={
+                location.pathname === "/ManangeForms" ? "mf" : "non_mf"
+              }
+              onClick={navHandle5}
+            >
+              <p
+                className={
+                  location.pathname === "/ManangeForms"
+                    ? "mf_text"
+                    : "nonmf_text"
+                }
+              >
+                Manage Forms
+              </p>
             </Grid>
             <Grid className="line4" />
 
-            <Grid className={location.pathname === "/ManangeLabs"  ? "ml" : "non_ml"} onClick={navHandle6}>
-              <p className={location.pathname === "/ManangeLabs" ? "ml_text" : "nonml_text"}>Manage Labs</p>
+            <Grid
+              className={location.pathname === "/ManangeLabs" ? "ml" : "non_ml"}
+              onClick={navHandle6}
+            >
+              <p
+                className={
+                  location.pathname === "/ManangeLabs"
+                    ? "ml_text"
+                    : "nonml_text"
+                }
+              >
+                Manage Labs
+              </p>
             </Grid>
             <Grid className="line5" />
 
-            <Grid className={location.pathname === "/Manangephysician" ? "mp" : "non_mp"} onClick={navHandle7}>
-              <p className={location.pathname === "/Manangephysician" ? "mp_text" : "nonmp_text"}>
+            <Grid
+              className={
+                location.pathname === "/Manangephysician" ? "mp" : "non_mp"
+              }
+              onClick={navHandle7}
+            >
+              <p
+                className={
+                  location.pathname === "/Manangephysician"
+                    ? "mp_text"
+                    : "nonmp_text"
+                }
+              >
                 Manage Physician
               </p>
             </Grid>
             <Grid className="line6" />
 
-            <Grid className={location.pathname === "/ManangePatients"  ? "mpa" : "non_mpa"} onClick={navHandle8}>
-              <p className={location.pathname === "/ManangePatients"  ? "mpa_text" : "nonmpa_text"}>
+            <Grid
+              className={
+                location.pathname === "/ManangePatients" ? "mpa" : "non_mpa"
+              }
+              onClick={navHandle8}
+            >
+              <p
+                className={
+                  location.pathname === "/ManangePatients"
+                    ? "mpa_text"
+                    : "nonmpa_text"
+                }
+              >
                 Manage Patient
               </p>
             </Grid>
             <Grid className="line7" />
 
-            <Grid className={location.pathname === "/SwabK" ? "swa" : "non_swa"} onClick={navHandle9}>
-              <p className={location.pathname === "/SwabK" ? "swa_text" : "nonswa_text"}>Swab KITS</p>
+            <Grid
+              className={location.pathname === "/SwabK" ? "swa" : "non_swa"}
+              onClick={navHandle9}
+            >
+              <p
+                className={
+                  location.pathname === "/SwabK" ? "swa_text" : "nonswa_text"
+                }
+              >
+                Swab KITS
+              </p>
             </Grid>
           </Grid>
         </Grid>
-        
+
         <Grid className="user_det">
-        <Grid className="user" />
-        <Grid className="userimg" >
-          <img
-            src={ell}
-            alt=""
-            style={{
-              position: "absolute",
-              left: "1550px",
-              right: "77.34",
-              top: "17%",
-              bottom: "14%",
-              border: "1px solid #EF9035",
-              borderRadius: "45px",
-              opacity: 2,
-            }}
-          />
+          <Grid className="user" />
+          <Grid className="userimg">
+            <img
+              src={ell}
+              alt=""
+              style={{
+                position: "absolute",
+                left: "1550px",
+                right: "77.34",
+                top: "17%",
+                bottom: "14%",
+                border: "1px solid #EF9035",
+                borderRadius: "45px",
+                opacity: 2,
+              }}
+            />
 
-          <img
-            src={userface}
-            alt=""
-            style={{
-              position: "absolute",
-              Width: "302px",
-              Height: "201px",
-              left: "1551px",
-              right: "49.48%",
-              top: "17%",
-              bottom: -"79%",
-              borderRadius: "52px",
-              opacity: "none",
-            }}
-          />
-          <input
-            type="text"
-            classname="name"
-            value="  Justin"
-            readonly
-            style={{
-              position: "absolute",
-              width: "100px",
-              left: "1635px",
-              right: "47.66%",
-              top: "25%",
+            <img
+              src={userface}
+              alt=""
+              style={{
+                position: "absolute",
+                Width: "302px",
+                Height: "201px",
+                left: "1551px",
+                right: "49.48%",
+                top: "17%",
+                bottom: -"79%",
+                borderRadius: "52px",
+                opacity: "none",
+              }}
+            />
+            <input
+              type="text"
+              classname="name"
+              value="  Justin"
+              readonly
+              style={{
+                position: "absolute",
+                width: "100px",
+                left: "1635px",
+                right: "47.66%",
+                top: "25%",
 
-              border: "0px",
-              opacity: "0.9",
+                border: "0px",
+                opacity: "0.9",
 
-              fontFamily: "Open Sans",
-              fontStyle: "normal",
-              fontWeight: "600",
-              fontSize: "16px",
-              lineHeight: "22px",
-              background: "none",
-              color: "#2C3E50",
-            }}
-          />
-          <input
-            type="text"
-            classname="name"
-            value="  Admin"
-            readonly
-            style={{
-              position: "absolute",
-              width: "100px",
-              left: "1634px",
+                fontFamily: "Open Sans",
+                fontStyle: "normal",
+                fontWeight: "600",
+                fontSize: "16px",
+                lineHeight: "22px",
+                background: "none",
+                color: "#2C3E50",
+              }}
+            />
+            <input
+              type="text"
+              classname="name"
+              value="  Admin"
+              readonly
+              style={{
+                position: "absolute",
+                width: "100px",
+                left: "1634px",
 
-              top: "55%",
+                top: "55%",
 
-              border: "0px",
-              opacity: "0.9",
+                border: "0px",
+                opacity: "0.9",
 
-              fontFamily: "Open Sans",
-              fontStyle: "normal",
-              fontWeight: "600",
-              fontSize: "16px",
-              lineHeight: "22px",
-              background: "none",
-              color: "#2C3E50",
-            }}
-          />
+                fontFamily: "Open Sans",
+                fontStyle: "normal",
+                fontWeight: "600",
+                fontSize: "16px",
+                lineHeight: "22px",
+                background: "none",
+                color: "#2C3E50",
+              }}
+            />
 
-          <img
-            src={downarrow}
-            alt=""
-            style={{
-              position: "absolute",
-              height: "7px",
-              left: "1710px",
-              top: "34%",
-              cursor: "pointer",
-            }}
-          />
-        </Grid>
-        <div className="noti" style={{position:'absolute', top:"-14px", left:"5px"}}>
-          <img
-            src={ell1}
-            alt=""
-            style={{
-              position: "absolute",
-              left: "1825px",
-              top: "48px",
-            }}
-          />
+            <img
+              src={downarrow}
+              alt=""
+              style={{
+                position: "absolute",
+                height: "7px",
+                left: "1710px",
+                top: "34%",
+                cursor: "pointer",
+              }}
+            />
+          </Grid>
+          <div
+            className="noti"
+            style={{ position: "absolute", top: "-14px", left: "5px" }}
+          >
+            <img
+              src={ell1}
+              alt=""
+              style={{
+                position: "absolute",
+                left: "1825px",
+                top: "48px",
+              }}
+            />
 
-          <img
-            src={bell1}
-            alt=""
-            style={{
-              position: "absolute",
-              left: "1838px",
-              top: "60px",
-            }}
-          />
+            <img
+              src={bell1}
+              alt=""
+              style={{
+                position: "absolute",
+                left: "1838px",
+                top: "60px",
+              }}
+            />
 
-          <img
-            src={ell2}
-            alt=""
-            style={{
-              position: "absolute",
-              left: "1848px",
-              top: "58px",
-            }}
-          />
-        </div>
+            <img
+              src={ell2}
+              alt=""
+              style={{
+                position: "absolute",
+                left: "1848px",
+                top: "58px",
+              }}
+            />
+          </div>
         </Grid>
       </div>
 
-
-      
-
-      <Grid className="con" >
-    
-   
-      </Grid>
-      <div >
-      </div>
+      <Grid className="con"></Grid>
+      <div></div>
       {/* <Grid className="footer">
         <p className="foot_text">
           Copyrights © 2022 <span style={{fontWeight:'bolder'}}>Genetix</span>. All Rights Reserved
